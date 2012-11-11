4b6405ef1837b6b1b0cbcb1fa5f2b94afe705401
cesar <cesars1687@hotmail.com>
2012-11-11 08:49:07 +0100

    modificando modelo alumno,curso,

--- application/models/alumno_model.php
+++ application/models/alumno_model.php
@@ -16,7 +16,7 @@ class Alumno_model extends CI_Model
     function notas($nombre)
     {
 
-        return $this->db->query('select cursos.*,asignatura.*,alumnos.*alu_asi.alu_asi_nota from cursos,asignatura,alumnos,alu_asi,curso_abierto where (alumnos.alu_nombres ="' . $nombre . '"  or alumnos.alu_apellidos ="' . $nombre . '")
+        return $this->db->query('select cursos.*,asignatura.*,alumnos.*,alu_asi.alu_asi_nota from cursos,asignatura,alumnos,alu_asi,curso_abierto where (alumnos.alu_nombres ="' . $nombre . '"  or alumnos.alu_apellidos ="' . $nombre . '")
                                  and alu_asi.alumnos_idAlumnos = alumnos.idAlumnos and alu_asi.asignatura_idAsignatura = asignatura.idAsignatura and asignatura.curso_abierto_idCurso_abierto = curso_abierto.idCurso_abierto and cursos.idCursos = curso_abierto.idCurso_abierto')->result();
     }
 }
